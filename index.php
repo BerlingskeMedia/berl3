fa4f73e6d32caf682c1d55d80ce98d729d5f4f03
Bo Skakke <bo@skakke.dk>
2016-03-04 13:35:45 +0100

    friday

--- index.php
+++ index.php
@@ -94,7 +94,7 @@ $campaignId = 'campaign-' . mt_rand(1,3);
 										<?php
 											$imgW='920'; 
 											$showImg = '1';
-											$class = ' fs262 mb border-bottom main-teaser';
+											$class = ' fs26 mb border-bottom main-teaser';
 											$imgRatio = '16:7';
 											$showSummary = '1';
 											$showRelated = '1';
@@ -518,7 +518,16 @@ $campaignId = 'campaign-' . mt_rand(1,3);
 
 <script src="/dist/scripts/main.js"></script>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-8992845-4', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 
 </body>
