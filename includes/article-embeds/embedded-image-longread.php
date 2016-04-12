5c2a192d15c2a36abf606a6c64e1599eef5e66f0
Bo Skakke <bo@skakke.dk>
2016-04-12 16:48:31 +0200

    tuesday

--- includes/article-embeds/embedded-image-longread.php
+++ includes/article-embeds/embedded-image-longread.php
@@ -1,12 +1,15 @@
 <figure class="article-image-embed-longread">
-	<a href="#" class="open-gallery"><img src="<? dummy("image@1200x,16:9") ?>" width="" height="" alt="" /></a>
+	<a href="#" class="open-gallery"><img src="<? dummy("image@1200x,16:9") ?>" width="" height="" alt="" />
+		<span class="gallery-counter">
+			<i class="bicon-camera"></i>
+			<span class="count">12</span>
+			<span class="show-all">Se alle</span>
+		</span>
+	</a>
 	<figcaption class="image-caption">
 		<? if (dumb_luck("50%")): ?><? dummy("text@teaser") ?><? endif ?> 
 			<div class="image-meta">
 				<span class="byline">Foto: <? dummy("text@author") ?></span>
-				<a href="#" class="open-gallery gallery-counter-small">
-					<span class="image-count"><em><i class="bicon-camera"></i> 12 -</em> Se alle</span>
-				</a>
 			</div>		
 	</figcaption>
 </figure>
