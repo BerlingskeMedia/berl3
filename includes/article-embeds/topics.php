e3227dffed0ec7ceb76300c89c8645900b055f2b
Bo Skakke <bo@skakke.dk>
2016-04-28 21:01:15 +0200

    sdf

--- includes/article-embeds/topics.php
+++ includes/article-embeds/topics.php
@@ -1,6 +1,9 @@
 <div class="article-topics small-title-left">
 	<div class="section-title ">Emner</div>
+		
 		<ul class="">		
+			<li class="fadeout"></li>
 			<? while (dumb_luck("1-6")): ?><li><a href="tpl-topic.php" title="Mere om [TOPIC]"><? dummy("text@topics") ?></a></li><? endwhile ?>
 		</ul>
+
 </div>
