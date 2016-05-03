af71ddfb671d59c97c21d5550e5dbfb024190e9f
Bo Skakke <bo@skakke.dk>
2016-05-03 18:13:59 +0200

    footer

--- includes/article/right-col-2.php
+++ includes/article/right-col-2.php
@@ -76,7 +76,7 @@
 	</div>
 	
 	<div class="col-xs-12 hidden-lg-up">
-		<div class="text-center mb ">
+		<div class="text-center mb like-berlingske like-berlingske-tablet ">
 			<span class="label-follow">Følg Berlingske: </span>
 			<a href="https://www.facebook.com/berlingske/?fref=ts" class="btn-share-sm-facebook"  target="_blank"><i class="bicon-facebook"></i></a>
 			<a href="https://twitter.com/berlingske?lang=da" class="btn-share-sm-twitter"  target="_blank"><i class="bicon-twitter"></i></a>
--- includes/follow-berlingske-buttons.php
+++ includes/follow-berlingske-buttons.php
@@ -1,4 +1,4 @@
-<div class="text-right mb toggle-loadmore-fade-inv like-berlingske hidden-md-down">
+<div class="mb toggle-loadmore-fade-inv like-berlingske like-berlingske-not-tablet ">
 	<span class="label-follow">Følg Berlingske: </span>
 	<a href="https://www.facebook.com/berlingske/?fref=ts" class="btn-share-sm-facebook"  target="_blank"><i class="bicon-facebook"></i></a>
 	<a href="https://twitter.com/berlingske?lang=da" class="btn-share-sm-twitter"  target="_blank"><i class="bicon-twitter"></i></a>
