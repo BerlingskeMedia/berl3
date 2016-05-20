6c6e18d07514d23f7855791d71b0b342a7ffa24b
Bo Skakke <bo@skakke.dk>
2016-05-20 14:36:20 +0200

    search

--- includes/search.php
+++ includes/search.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class=" col-xs-12 clearfix">
 				<a href="#" class="toggle-search"><i class="bicon-cancel"></i></a>
-				<form action="POST" class="global-search" action="tpl-search.php">
+				<form method="POST" class="global-search" action="tpl-search.php">
 					<input type="search" placeholder="Søgeord" id="site-search">
 					<button class="btn"><i class="bicon-search"></i></button>
 				</form>
