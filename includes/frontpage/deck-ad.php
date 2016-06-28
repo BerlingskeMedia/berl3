2403b9a36dd8ddbe6cecdfe2cca8ecb5fa8a016c
Bo Skakke <bo@skakke.dk>
2016-06-28 13:47:21 +0200

    remove banners on sm

--- includes/frontpage/deck-ad.php
+++ includes/frontpage/deck-ad.php
@@ -3,7 +3,7 @@
 		<div class="col-tags col-border-right-lg-up">
 
 		</div>
-		<div class="col-primary-wide banner ">
+		<div class="col-primary-wide banner hidden-sm-down">
 			<div class="trade-container-wide">
 				<? dummy("ad@930x180") ?>				
 			</div>
