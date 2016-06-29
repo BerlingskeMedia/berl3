9c233e9e7128a9b3a65ba5a4a50e76d0e9ccfae5
Bo Skakke <bo@skakke.dk>
2016-06-29 17:25:02 +0200

    teasers links

--- includes/article-embeds/link.php
+++ includes/article-embeds/link.php
@@ -6,20 +6,166 @@
 			<a href="tpl-article.php"><img src="<? dummy("image@100x,3:2") ?>" width="" height="" alt="" /></a>
 		</figure>
 		<div class="teaser-body">
-			<span>Læs også</span>
+			<?php $labelType = 'yellow';
+			$labelText = 'Breaking';  ?>
+			<?php if($labelType): ?>
+			
+			
+				<?php include('includes/frontpage/teasers/labels.php'); ?>	
+			
+			
+			<?php else: ?>
+			<span class="read-also">Læs også</span>
+			<?php endif; ?>
+			
 			<h2 class="header"><a href="tpl-article.php"><? dummy("text@headline-b") ?></a></h2>
 		</div>
 	</article>
 </aside>
-<p><? dummy("text@paragraph") ?></p>
+<p><? dummy("text@teaser") ?></p>
+
+
 <aside class="article-embedded-link">
 	<article>
 		<figure>
 			<a href="tpl-article.php"><img src="<? dummy("image@100x,3:2") ?>" width="" height="" alt="" /></a>
 		</figure>
 		<div class="teaser-body">
-			<span>Se galleri</span>
+			<?php $labelType = 'live';
+			$labelText = '<i class="bicon-record"></i> Live';  ?>
+			<?php if($labelType): ?>
+			
+			
+				<?php include('includes/frontpage/teasers/labels.php'); ?>	
+			
+			
+			<?php else: ?>
+			<span class="read-also">Læs også</span>
+			<?php endif; ?>
+			
 			<h2 class="header"><a href="tpl-article.php"><? dummy("text@headline-b") ?></a></h2>
+		</div>
+	</article>
+</aside>
+<p><? dummy("text@teaser") ?></p>
+<aside class="article-embedded-link">
+	<article>
+		<figure>
+			<a href="tpl-article.php"><img src="<? dummy("image@100x,3:2") ?>" width="" height="" alt="" /></a>
+		</figure>
+		<div class="teaser-body">
+			<?php $labelType = 'red';
+			$labelText = 'Opdateres';  ?>
+			<?php if($labelType): ?>
+			
+			
+				<?php include('includes/frontpage/teasers/labels.php'); ?>	
+			
+			
+			<?php else: ?>
+			<span class="read-also">Læs også</span>
+			<?php endif; ?>
+			
+			<h2 class="header"><a href="tpl-article.php"><? dummy("text@headline-b") ?></a></h2>
+		</div>
+	</article>
+</aside>
+<p><? dummy("text@teaser") ?></p>
+
+<aside class="article-embedded-link">
+	<article>
+		<figure>
+			<a href="tpl-article.php"><img src="<? dummy("image@100x,3:2") ?>" width="" height="" alt="" /></a>
+		</figure>
+		<div class="teaser-body">
+			<?php $labelType = 'red';
+			$labelText = 'Lige nu';  ?>
+			<?php if($labelType): ?>
+			
+			
+				<?php include('includes/frontpage/teasers/labels.php'); ?>	
+			
+			
+			<?php else: ?>
+			<span class="read-also">Læs også</span>
+			<?php endif; ?>
+			
+			<h2 class="header"><a href="tpl-article.php"><? dummy("text@headline-b") ?></a></h2>
+		</div>
+	</article>
+</aside>
+<p><? dummy("text@teaser") ?></p>
+
+<aside class="article-embedded-link">
+	<article>
+		<figure>
+			<a href="tpl-article.php"><img src="<? dummy("image@100x,3:2") ?>" width="" height="" alt="" /></a>
+		</figure>
+		<div class="teaser-body">
+			<?php $labelType = 'red';
+			$labelText = 'Nyhed på vej';  ?>
+			<?php if($labelType): ?>
+			
+			
+				<?php include('includes/frontpage/teasers/labels.php'); ?>	
+			
+			
+			<?php else: ?>
+			<span class="read-also">Læs også</span>
+			<?php endif; ?>
+			
+			<h2 class="header"><a href="tpl-article.php"><? dummy("text@headline-b") ?></a></h2>
+		</div>
+	</article>
+</aside>
+<p><? dummy("text@teaser") ?></p>
+
+<aside class="article-embedded-link">
+	<article>
+		<figure>
+			<a href="tpl-article.php"><img src="<? dummy("image@100x,3:2") ?>" width="" height="" alt="" /></a>
+		</figure>
+		<div class="teaser-body">
+			<?php $labelType = 'blue';
+			$labelText = 'Tophistorie';  ?>
+			<?php if($labelType): ?>
+			
+			
+				<?php include('includes/frontpage/teasers/labels.php'); ?>	
+			
+			
+			<?php else: ?>
+			<span class="read-also">Læs også</span>
+			<?php endif; ?>
+			
+			<h2 class="header"><a href="tpl-article.php"><? dummy("text@headline-b") ?></a></h2>
+		</div>
+	</article>
+</aside>
+<p><? dummy("text@teaser") ?></p>
+
+<aside class="article-embedded-link">
+	<article>
+		<figure>
+			<a href="tpl-article.php"><img src="<? dummy("image@100x,3:2") ?>" width="" height="" alt="" /></a>
+		</figure>
+		<div class="teaser-body">
+			<span class="read-also">Se galleri</span>
+			<h2 class="header"><a href="tpl-article.php"><i class="bicon-camera"></i> <? dummy("text@headline-b") ?></a></h2>
+		</div>
+	</article>
+</aside>
+
+<p><? dummy("text@teaser") ?></p>
+
+<aside class="article-embedded-link">
+	<article>
+		<figure>
+			<a href="tpl-article.php"><img src="<? dummy("image@100x,3:2") ?>" width="" height="" alt="" /></a>
+		</figure>
+		<div class="teaser-body">
+			<span class="read-also">Se video</span>
+			<h2 class="header"><a href="tpl-article.php"><i class="bicon-play"></i> <? dummy("text@headline-b") ?></a></h2>
 		</div>
 	</article>
 </aside>
--- includes/article-embeds/newsletter-embedded.php
+++ includes/article-embeds/newsletter-embedded.php
@@ -11,7 +11,7 @@
 
 		<form action="" class="form-embedded-newsletter  form-round-oneliner">
 			<div class="clearfix">
-				<!-- the class .newsletter-email is uded by js to slide down breaking cheackbox->
+				<!-- the class .newsletter-email is uded by js to slide down breaking cheackbox -->
 				<input type="email" placeholder="Din e-mailadresse" required class="newsletter-email">
 				<button class="btn  btn-black">OK</button>
 
@@ -58,7 +58,7 @@
 	<p class="mb-05"><small >Udkommer hver morgen!</small></p>
 	<form action="" class="form-embedded-newsletter form-round-oneliner">
 		<div class="clearfix">
-			<!-- the class .newsletter-email is uded by js to slide down breaking cheackbox->
+			<!-- the class .newsletter-email is uded by js to slide down breaking cheackbox -->
 			<input type="email" placeholder="Din e-mailadresse" required class="newsletter-email">
 			<button class="btn  btn-blue">OK</button>
 		</div>
