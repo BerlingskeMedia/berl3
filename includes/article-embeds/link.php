2a73a29fcab520e68cfde1e32be85b0658d6bbad
Bo Skakke <bo@skakke.dk>
2016-06-29 17:37:29 +0200

    teasers

--- includes/article-embeds/link.php
+++ includes/article-embeds/link.php
@@ -170,4 +170,21 @@
 	</article>
 </aside>
 
+<aside class="article-embedded-link">
+	<article>
+		<figure>
+			<a href="tpl-article.php"><img src="<? dummy("image@100x,3:2") ?>" width="" height="" alt="" /></a>
+		</figure>
+		<div class="teaser-body">
+			
+			<span class="read-also">Læs også</span>
+			
+			
+			<h2 class="header"><a href="tpl-article.php"><? dummy("text@headline-b") ?></a></h2>
+			<span class="subscriber-notice">Abonnement</span>
+		</div>
+	</article>
+</aside>
+<p><? dummy("text@teaser") ?></p>
+
 <?php endif; ?>
