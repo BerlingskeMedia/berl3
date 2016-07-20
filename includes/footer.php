56a3a3bb724776fb88a86abcb063e723a8014c07
Bo Skakke <bo@skakke.dk>
2016-07-20 10:03:05 +0200

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,131 +3,106 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12 ">
-					<ul class="list-footer-menu list-reset list-sm mb-0 ">
-						<li><a href="#"><b class="bicon-home"></b> Forside</a></li>
-						
- 
-						<li><a href="#">Business</a></li>
-						<li><a href="#">Politiko</a></li>
-						<li class="active"><a href="#">Debat</a></li>
-						<li><a href="#">Global</a></li>
-						<li><a href="#">Tech</a></li>
-						<li><a href="#">Viden</a></li>
-						<li><a href="#">Kultur</a></li>
-						<li><a href="#">AoK</a></li>
-						<li><a href="#">Livsstil</a></li>
-						<li><a href="#">Rejseliv</a></li>
-						<li><a href="#">Sport</a></li>
-						<li class="hidden-lg-up"><a href="#"><b class="bicon-menu"></b> Indhold</a></li>
-					</ul>
+					<span class="site-logo">Berlingske</span>
+					<a href="#" class="top-link">Til toppen <i class="bicon-angle-up"></i></a>
 				</div>
 			</div>
 		</div>
 	</div>
 
+	
 	
 	
 	<div class="footer-middle ">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-3">
+				<div class="col-md-4 ">
 					<div class="row">
-						<div class="col-md-12 hidden-sm-down ">
-							
-Berlingske
-Pilestræde 34
-DK-1147 København K
-Danmark
-
-Tlf: 33 75 75 75
-Fax: 33 75 20 20
-
-Kontakt os
-
-CVR.nr.: 29 20 73 13	
-								
-							
+						<div class="col-md-5 col-xs-5 col-xs-offset-4 col-md-offset-0">
+							<img src="/dist/images/vaabenskjold-footer.png" alt="" class="mb">
+							<p class="text-center">Udgivet siden 1749</p>
 						</div>
-						<div class="col-md-12 col-sm-12 col-xs-12 mb-sm">
-							<div class="border-bottom-sm ">
-							<p class="hidden-sm-down"><strong >Følg Berlingske</strong></p>
+						<div class="col-md-7 col-xs-12 mb">
+							<p>Berlingske<br>
+							Pilestræde 34<br>
+							DK-1147 København K<br>
+							Danmark</p>
 
-								<ul class="footer-list list-social-icons columns-lg-up align-center-sm">
-									
-									<li>
-										<a href="#"><i class="bicon-facebook"></i> <span>Facebook</span></a>
-									</li>
-									<li>
-										<a href="#"><i class="bicon-twitter"></i> <span>Twitter</span></a>
-									</li>
-									
-						
-									<li>
-										<a href="#"><i class="bicon-instagram"></i> <span>Instagram</span></a>
-									</li>
-									<li>
-										<a href="#"><i class="bicon-mail"></i> <span>Nyhedsbreve</span></a>
-									</li>
-								</ul>
-								
-							</div>
+							<p>Tlf: 33 75 75 75<br>
+							Fax: 33 75 20 20</p>
+
+							<ul class="footer-list">	
+								<li><a href="#">Kontakt os</a></li>
+								<li><a href="#">Send pressemeddelelse</a></li>
+								<li><a href="#">Kundeservice</a></li>
+							</ul>
+							
+							<p>CVR.nr.: 29 20 73 13	</p>		
 						</div>
 					</div>
 					
 					
+						
 				</div>
-				<div class="col-md-9 col">
+				
+				<div class="col-md-8">
 					<div class="row">
-						<div class="col-md-4 col-sm-6 col-xs-6 mb-sm">
-							<p><strong>Om os</strong></p>
-							<ul class="footer-list">
-								<li><a href="#" title="#">Kontakt os</a></li>
-								<li><a href="#" title="#">Send pressemeddelelse</a></li>
-								<li><a href="#" title="#">Fejl og fakta</a></li>
-								<li><a href="#" title="#">Redaktionelt regnskab</a></li>
-								<li><a href="#" title="#">Etiske regler</a></li>
-								<li><a href="#" title="#">Ledige stillinger</a></li>
-								<li><a href="#" title="#">Generelle handelsbetingelser</a></li>
-								
-							</ul>
+						<div class="col-md-4  mb">
+								<strong>Om Berlingske</strong>
+								<ul class="footer-list">
+									<li><a href="#">Redaktionelt regnskab</a></li>
+									<li><a href="#">Fejl og fakta</a></li>
+									<li><a href="#">Etiske regler</a></li>
+									<li><a href="#">Ledige stillinger</a></li>
+									<li><a href="#">Annoncering</a></li>
+								</ul>
 						</div>
-						<div class="col-md-4 col-sm-6 col-xs-6  mb-sm">
-							<p><strong>Service</strong></p>
-							<ul class="footer-list">
-								<li><a href="#" title="#">Køb abonnement</a></li>
-								<li><a href="#" title="#">Læs e-avisen</a></li>
-								<li><a href="#" title="#">Er avisen ikke kommet?</a></li>
-								<li><a href="#" title="#">Annoncesalg</a></li>
-							</ul>
+
+						<div class="col-md-4  mb">
+								<strong>Kundeservice</strong>
+								<ul class="footer-list">
+									<li><a href="#">Kontakt kundeservice</a></li>
+									<li><a href="#">Er avisen ikke kommet?</a></li>
+									<li><a href="#">E-avis</a></li>
+									<li><a href="#">Køb abonnement </a></li>
+								</ul>
 						</div>
-						<div class="col-md-4 hidden-sm-down">
-							<p><strong>Tjenester</strong></p>
-							<ul class="footer-list">
-								<li><a href="#" title="#">RSS</a></li>
-								<li><a href="#" title="#">Apps og mobile tjenester</a></li>
-								<li><a href="#" title="#">Foto privatsalg</a></li>
-								<li><a href="#" title="#">Sitemap</a></li>
-								<li><a href="#" title="#">Artikel arkiv indeks</a></li>
-							</ul>
+
+						<div class="col-md-4  mb">
+								<strong>Tjenester</strong>
+								<ul class="footer-list">
+									<li><a href="#">Apps </a></li>
+									<li><a href="#">Foto - privat salg</a></li>
+									<li><a href="#">Sitemap</a></li>
+									<li><a href="#">Artikelarkiv</a></li>
+									<li><a href="#">Nyhedsbreve</a></li>
+								</ul>
 						</div>
 					</div>
 				</div>
 				
+					
+					
+				</div><!-- row -->
+				<div class="row">
+					<div class="col-xs-12 text-center social-buttons">
+						<p><strong>Følg Berlingske </strong>
+						<a href="#" class="btn-circle"><i class="bicon-facebook"></i></a>
+						<a href="#" class="btn-circle"><i class="bicon-twitter"></i></a>
+						<a href="#" class="btn-circle"><i class="bicon-instagram"></i></a></p>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-xs-12 text-center" >
+						
+						<p>Copyright &copy; Berlingske Media.<span class="divider">|</span> <a href="#" target="blank">Ophavsret og vilkår</a> <span class="divider">|</span> <a href="#">Generelle handelsbetingelser</a> <span class="divider">|</span> <a href="#" target="_blank">Cookie- og Privatlivspolitik</a></p>
+					</div>
+				</div>			
 			</div>	
 		</div>
-	</div>
-	
-	<div class="footer-bottom">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12">
-					
-					<!-- <p>Tlf. 33 75 75 75<br>
-									Fax: 33 75 20 20</p> --> 
-					<p>Copyright &copy; Berlingske Media. <a href="#" target="blank">Ophavsret og vilkår</a> <span class="hidden-md-down">|</span> <a href="#">Generelle handelsbetingelser</a> <span class="hidden-md-down">|</span> <a href="#" target="_blank">Cookie- og Privatlivspolitik</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
 </div>
+	
+	
+
 
