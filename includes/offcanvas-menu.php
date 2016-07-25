fae7ff9fa04835dc5cb19d3db7532c257982622f
Bo Skakke <bo@skakke.dk>
2016-07-25 13:23:38 +0200

    menu

--- includes/offcanvas-menu.php
+++ includes/offcanvas-menu.php
@@ -10,12 +10,19 @@
 					<ul class="sitemap-parent">
 						<? while (dumb_luck("7")): ?>
 						<li>
-							<a href="tpl-section.php"><? dummy("text@category") ?></a>
+							<a href="tpl-section.php" class="sitemap-parent-link"><? dummy("text@category") ?></a>
 							<ul class="sitemap-child">
 								<? while (dumb_luck("3-9")): ?>
 								<li><a href="tpl-section.php"><? dummy("text@category") ?></a></li>
 								<? endwhile ?>
 							</ul>
+						</li>
+						<? endwhile ?>
+					</ul>
+					<ul class="sitemap-small">
+						<? while (dumb_luck("2-5")): ?>
+						<li>
+							<a href="#"><? dummy("text@category") ?></a>
 						</li>
 						<? endwhile ?>
 					</ul>
