85aa1ae09bdd2297ca056baff4ba57c2b1843e3d
Bo Skakke <bo@skakke.dk>
2016-08-09 16:05:21 +0200

    playicon

--- includes/article/paywall.php
+++ includes/article/paywall.php
@@ -61,8 +61,13 @@
 <?php elseif($paywall === 'video-soft'): ?>
 
 <div class="paywall paywall-video ">
+	
+
+		<span class="icon"><i class="bicon-play"></i></span>
 	<div class="row">
+
 		<div class="col-xs-12 " >
+			
 			<h2>Vi er i borgerligt godt humør i dag!</h2>
 			<p>Denne video er kun for abonnenter, <strong>men vi synes du skal se den alligevel</strong>. Prøv vores <a href="#" class="link-blue">digitale abonnement</a> gratis i en måned.</p>
 			<div class="row">
@@ -77,9 +82,10 @@
 <?php elseif($paywall === 'video-hard'): ?>
 
 <div class="paywall paywall-video ">
+
 	<div class="row">
 		<div class="col-xs-12 " >
-			
+			<span class="icon"></span>		
 			<p class="small mb-05" >Videoen kræver abonnement</p>
 			<h2>Borgerlig journalistik 24/7/365 </h2>		
 			<p>Danmarks borgerlige avis holder aldrig lukket, og med et <a href="#" class="link-blue">digitalt abonnement</a> kan du altid læse om den verden vi lever i, mens den drejer rundt.</p>
