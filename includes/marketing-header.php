a27cd189e3d0b6df3511e94259ddd565d6aef913
Bo Skakke <bo@skakke.dk>
2016-08-12 10:04:25 +0200

    paywall and marketing

--- includes/marketing-header.php
+++ includes/marketing-header.php
@@ -1,19 +1,11 @@
-<div class="header-marketing hidden-sm-down">
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 text-center">
-				<a href="#"><strong>Prøv Berlingske i 3 mdr</strong> - Trykt avis, e-avis, og digital adgang hele ugen. Kun 249,-/ md. <strong>Køb nu!</strong> 
-								<span class="gray">Samlet engangspris 747,- Abonnementet fortsætter til normalpris indtil det opsiges</span>		</a>
-			</div>
 
-		</div>
-	</div>	
-</div>
 <div class="header-marketing-2 hidden-sm-down">
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 text-center">
-				<a href="#"><strong>Prøv Berlingske i 3 mdr og spar 499,- </strong> Inkl. trykt avis, e-avis, og digital adgang hele ugen. <strong>Køb nu! <i class="bicon-angle-right"></i></strong> </a>
+				<a href="#">
+				<strong>Berlingske hver dag i 3 mdr. for 249 kr./md.</strong> Samlet pris 747 kr. <strong class="link-blue">Bestil her <i class="bicon-angle-right"></i></strong>
+				</a>
 			</div>
 
 		</div>
