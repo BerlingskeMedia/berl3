8205f6684c6c990410d83ba841d1f3c3f959e710
Bo Skakke <bo@skakke.dk>
2016-09-08 15:26:09 +0200

    consumersales grid

--- includes/frontpage/deck-consumersales-2.php
+++ includes/frontpage/deck-consumersales-2.php
@@ -1,120 +1,117 @@
-<div class="deck deck-theme">
-	<div class="theme-container theme-consumersales">
-		<div class="col-tags ">
-				<div class="section-title">Shop</div>
-				
-					<a href="tpl-section.php" class="font-s1 mb-sm-down" style="display: block; font-size: 1.8rem">Få flere bedre oplevelser.</a>
-			
-			
-		</div>
+<div class="deck deck-theme theme-container theme-consumersales">
+	<div class="grid-col-left col-xs-2">
+			<div class="section-title">
+				Shop
+			</div>
+			<a href="tpl-section.php" class="font-s1 mb-sm-down" style="display: block; font-size: 1.8rem">Få flere bedre oplevelser.</a>
+	</div>
 
-		
-		<div class="col-primary">
-			<div class="row flex-row ">
-				<div class="col-md-4 col-xs-6 ">
+	<div class="grid-col-middle-wide col-xs-10">
+		<div class="row flex-row ">
+			<div class="col-md-4 col-xs-6 ">
 
-						<?php
-							$imgW='538'; 
-							$showImg = '1';
-							$class = 'teaser-consumersales item-teaser  mb-sm-down';
-							$imgRatio = '1:1';
-							$imgSrc = '/!consumersales';
-							$showSummary = false;
-							$showHeader = true;
-							$showRelated = false;
-							$showCategory = false;
-							$teaserLabel = false;
-							$showComments = false;
-							$showTrumpet = false;
-							$headerSrc = 'consumersales-title';
-							$summarySrc = 'consumersales-subtitle';
-							$showPrice = true;
-							include('includes/frontpage/teasers/teaser.php') ?>
-					</div>
-					
-					<div class="col-md-4 col-xs-6">
-							
-						<?php
-							$imgW='538'; 
-							$showImg = '1';
-							$class = 'teaser-consumersales item-teaser ';
-							$imgRatio = '1:1';
-							$imgSrc = '/!consumersales';
-							$showSummary = false;
-							$showHeader = true;
-							$showRelated = false;
-							$showCategory = false;
-							$teaserLabel = false;
-							$showComments = false;
-							$showTrumpet = false;
-							$headerSrc = 'consumersales-title';
-							$summarySrc = 'consumersales-subtitle';
-							$showPrice = true;
-							include('includes/frontpage/teasers/teaser.php') ?>
-					</div>
-
-
-					<div class="col-md-4 col-xs-12 flex-row">
-						
-						<div class="row flex-row">
-							<div class="col-xs-6 flex-row">
-								<div class="teaser-consumersales item-teaser teaser-consumersales-fixed" style="padding: 1rem;">
-									<h2 class="fixed-header mb text-center font-s1" style="font-weight: 400; color: #fff;" >Vinreolen</h2>
-								</div>						
-							</div>
-
-							<div class="col-xs-6">
-								
-								
-									<?php
-										$imgW='538'; 
-										$showImg = '1';
-										$class = 'teaser-consumersales item-teaser  margin125  ';
-										$imgRatio = '1:1';
-										$imgSrc = '/!consumersales';
-										$showSummary = false;
-										$showHeader = true;
-										$showRelated = false;
-										$showCategory = false;
-										$teaserLabel = false;
-										$showComments = false;
-										$showTrumpet = false;
-										$headerSrc = 'consumersales-title';
-										$summarySrc = 'consumersales-subtitle';
-										$showPrice = true;
-										include('includes/frontpage/teasers/teaser.php') ?>
-								
-								
-										<?php
-											$imgW='538'; 
-											$showImg = '1';
-											$class = 'teaser-consumersales item-teaser ';
-											$imgRatio = '1:1';
-											$imgSrc = '/!consumersales';
-											$showSummary = false;
-											$showHeader = true;
-											$showRelated = false;
-											$showCategory = false;
-											$teaserLabel = false;
-											$showComments = false;
-											$showTrumpet = false;
-											$headerSrc = 'consumersales-title';
-											$summarySrc = 'consumersales-subtitle';
-											$showPrice = true;
-											include('includes/frontpage/teasers/teaser.php') ?>
-								
-
-							</div>
-						</div>	
-					</div>
-
-					
-
-
-
-					
+					<?php
+						$imgW='538'; 
+						$showImg = '1';
+						$class = 'teaser-consumersales item-teaser  mb-sm-down';
+						$imgRatio = '1:1';
+						$imgSrc = '/!consumersales';
+						$showSummary = false;
+						$showHeader = true;
+						$showRelated = false;
+						$showCategory = false;
+						$teaserLabel = false;
+						$showComments = false;
+						$showTrumpet = false;
+						$headerSrc = 'consumersales-title';
+						$summarySrc = 'consumersales-subtitle';
+						$showPrice = true;
+						include('includes/frontpage/teasers/teaser.php') ?>
 				</div>
-		</div>
+				
+				<div class="col-md-4 col-xs-6">
+						
+					<?php
+						$imgW='538'; 
+						$showImg = '1';
+						$class = 'teaser-consumersales item-teaser ';
+						$imgRatio = '1:1';
+						$imgSrc = '/!consumersales';
+						$showSummary = false;
+						$showHeader = true;
+						$showRelated = false;
+						$showCategory = false;
+						$teaserLabel = false;
+						$showComments = false;
+						$showTrumpet = false;
+						$headerSrc = 'consumersales-title';
+						$summarySrc = 'consumersales-subtitle';
+						$showPrice = true;
+						include('includes/frontpage/teasers/teaser.php') ?>
+				</div>
+
+
+				<div class="col-md-4 col-xs-12 flex-row">
+					
+					<div class="row flex-row">
+						<div class="col-xs-6 flex-row">
+							<div class="teaser-consumersales item-teaser teaser-consumersales-fixed" style="padding: 1rem;">
+								<h2 class="fixed-header mb text-center font-s1" style="font-weight: 400; color: #fff;" >Vinreolen</h2>
+								<a href="#" style="position: absolute; left: 0;	right: 0; top: 0;bottom: 0"></a>
+							</div>						
+						</div>
+
+						<div class="col-xs-6">
+							
+						
+							<?php
+								$imgW='538'; 
+								$showImg = '1';
+								$class = 'teaser-consumersales item-teaser  margin125  ';
+								$imgRatio = '1:1';
+								$imgSrc = '/!consumersales';
+								$showSummary = false;
+								$showHeader = true;
+								$showRelated = false;
+								$showCategory = false;
+								$teaserLabel = false;
+								$showComments = false;
+								$showTrumpet = false;
+								$headerSrc = 'consumersales-title';
+								$summarySrc = 'consumersales-subtitle';
+								$showPrice = true;
+								include('includes/frontpage/teasers/teaser.php') ?>
+						
+						
+							<?php
+								$imgW='538'; 
+								$showImg = '1';
+								$class = 'teaser-consumersales item-teaser ';
+								$imgRatio = '1:1';
+								$imgSrc = '/!consumersales';
+								$showSummary = false;
+								$showHeader = true;
+								$showRelated = false;
+								$showCategory = false;
+								$teaserLabel = false;
+								$showComments = false;
+								$showTrumpet = false;
+								$headerSrc = 'consumersales-title';
+								$summarySrc = 'consumersales-subtitle';
+								$showPrice = true;
+								include('includes/frontpage/teasers/teaser.php') ?>
+						
+
+						</div>
+					</div>	
+				</div>
+
+				
+
+
+
+				
+			</div>
 	</div>
 </div>
 
