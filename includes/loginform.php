ba3e3f1faf5d6358289e823802b1956058de2d87
Bo Skakke <bo@skakke.dk>
2016-10-05 14:24:27 +0200

    styled login box

--- includes/loginform.php
+++ includes/loginform.php
@@ -7,48 +7,67 @@
 
 		<div class="site-logo text-center mb">Berlingske</div>
 
+		<p class="small text-center">Log ind eller køb et abonnement for at få adgang til alt indhold på Berlingske.dk</p>
 		  
-		<p class="text-center">Log ind og få adgang til alt indhold på Berlingske.dk</p>
+		<div class="login--form">
+			
+			<ul class="login--form__tabs">
+				<li class="login--tab__active">
+					<a href="#">Log ind</a>
+				</li>
+				<li class="login--tab">
+					<a href="http://abonnement.b.dk/berlingske-to-produkter-paywallbanner/?utm_source=paywall&utm_medium=paywall_bdk&utm_term=AL-151222-CP-WKLEDIT1PW&utm_content=loginpopup&utm_campaign=abonnementer_berlingske" target="_blank">Køb abonnement</a>
+				</li>
+			</ul>
+
+			<div class="login--form__inner">
+				<form action="" class="form " >
+					<div class="alert alert-danger">
+						<p>Forkert e-mailadresse og/eller adgangskode</p>
+					</div>
+					<fieldset class="form-group">
+						<input type="email" class="form-control" id="login-email" placeholder="E-mailadresse" autocomplete="on" >
+						<div class="msg-error"><i class="bicon-angle-up"></i> Du skal indtaste en gyldig e-mailadresse</div>
+					</fieldset>
+
+					<fieldset class="form-group">
+						
+						<input type="password" class="form-control" id="login-password" placeholder="Adgangskode" >
+					</fieldset>
+
+					<div class="row">
+						<div class="col-xs-5">
+							<div class="checkbox checkbox-styled checkbox-styled-dark ">
+								<label>
+									<input type="checkbox" class="" checked=""> <span>Husk mig</span>
+								</label>
+							</div>	
+						</div>
+						<div class="col-xs-7 text-right">
+							<a href="#" class="forgot-password toggle-forgot-password">Glemt adgangskode?</a>
+						</div>
+					</div>
+						
+					
+					<button class="btn  btn-blue btn-block mt ">Log ind</button>
+					
+					<div class="msg-error hidden"><i class="bicon-angle-up"></i> Du skal indtaste en gyldig e-mailadresse</div>
+				</form>	
+			</div>
+
+			<p class="small"><strong>Allerede abonnent? </strong>Har du et avisabonnement men mangler brugernavn og adgangskode? <a href="http://b.kundeunivers.dk/allerede-abonnent?[INSERT LINK PARAMETERS]">Opret profil <i class="bicon-angle-right"></i></a></p>
+			<p class="small"><strong>Brug for hjælp?</strong> Kontakt Kunceservice på 33 75 35 00.</p>
+
+
+		</div>
 		
-		<form action="" class="form ">
-			<div class="alert alert-danger">
-				<p>Forkert e-mailadresse og/eller adgangskode</p>
-			</div>
-			<fieldset class="form-group">
-				<input type="email" class="form-control" id="login-email" placeholder="E-mailadresse" autocomplete="on" >
-				<div class="msg-error"><i class="bicon-angle-up"></i> Du skal indtaste en gyldig e-mailadresse</div>
-			</fieldset>
-
-			<fieldset class="form-group">
-				
-				<input type="password" class="form-control" id="login-password" placeholder="Adgangskode" >
-			</fieldset>
-
-			<div class="row">
-				<div class="col-xs-5">
-					<div class="checkbox checkbox-styled checkbox-styled-dark ">
-						<label>
-							<input type="checkbox" class="" checked=""> <span>Husk mig</span>
-						</label>
-					</div>	
-				</div>
-				<div class="col-xs-7 text-right">
-					<a href="#" class="forgot-password toggle-forgot-password">Glemt adgangskode?</a>
-				</div>
-			</div>
-				
-			
-			<button class="btn  btn-blue btn-block mt mb">Log ind</button>
-			
-			<p class="small">Har du allerede et avisabonnement men mangler brugernavn og adgangskode? <a href="http://b.kundeunivers.dk/allerede-abonnent?utm_source=paywall&utm_medium=paywall_bdk&utm_term=AL-151222-CP-WKLEDIT1PW&utm_content=login&utm_campaign=abonnementer_berlingske">Få hjælp her <i class="bicon-angle-right"></i></a></p>
-			<div class="msg-error hidden"><i class="bicon-angle-up"></i> Du skal indtaste en gyldig e-mailadresse</div>
-		</form>
+		
 		</div>
 		<div class="slide-2">
 			<div class="forgot-password-container">
 				<h2>Glemt adgangskode</h2>
 
-				<p>Indtast den e-mailadresse, du har oprettet dig med på Berlingske.dk, så sender vi en mail med information om, hvordan du ændrer din adgangskode.</p>
+				<p>Indtast din e-mailadresse, så sender vi dig en e-mail med information om, hvordan du ændrer din adgangskode.</p>
 				<form action="" class="form">
 					<fieldset class="form-group">
 						<input type="email" class="form-control" id="login-email" placeholder="E-mailadresse" autocomplete="on" >
@@ -60,6 +79,7 @@
 					<p>Vi har nu sendt dig en e-mail med instruktioner om, hvordan du ændrer din adgangskode.</p>
 				</div>
 				<p><small><a href="#" class="close-reminder toggle-forgot-password"><i class="bicon-angle-left"></i> Tilbage til log ind</a></small></p>
+				<p class="small"><strong>Brug for hjælp?</strong> Kontakt Kunceservice på 33 75 35 00.</p>
 			</div>
 		</div>
 
--- test.php
+++ test.php
@@ -25,66 +25,8 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12">
-			<? while (dumb_luck("14")): ?><p><? dummy("text@paragraph") ?></p><? endwhile ?>      
-			<aside class="article-embed-pull-left newsletter-custom newsletter-custom-embedded continue-reading-newsletter">
-				
-					<div class="newsletter-container">
-						<div class="newsletter-main-title">
-							Få <b>Berlingske Kultur</b> i din indbakke
-						</div>
-
-						<form  class="checkbox-styled checkbox-styled-large ">
-							<div class="main-newsletter">
-								<input type="email" name="email" placeholder="Indtast din email">	
-							</div>
-							
-							<div class="hidden-newsletters">
-								<div class="other-newsletters">Tilmeld mig også:</div>
-								
-								<div class="newsletter-item">
-									<label for="newsletter-2">
-										<input type="checkbox" name="subscriptions" id="newsletter-2" class="newsletter-checkbox" value="" checked>
-										<div class="newsletter-title">
-											Berlingske <b>Morgen</b>
-										</div>
-										<div class="newsletter-description">
-											<? dummy("text@short-teaser") ?>
-										</div>
-									</label>
-								</div>
-								<div class="newsletter-item">
-									<label for="newsletter-3">
-										<input type="checkbox" name="subscriptions" id="newsletter-3" class="newsletter-checkbox" value="" checked>
-										<div class="newsletter-title">
-											Berlingske <b>Breaking</b>
-										</div>
-										<div class="newsletter-description">
-											<? dummy("text@short-teaser") ?>
-										</div>
-									</label>
-								</div>
-							</div>
-							<div class="newsletter-submit">
-								<button href="#" class="btn btn-blue btn-sm custom-newsletter-submit">Tilmeld</button>
-							</div>
-							
-							
-						</form>
-					</div>
-				
-				<div class="newsletter-final ">
-					<div class="newsletter-final_title">TAK!</div>
-					<div class="newsletter-final_description">
-						Du er nu tilmeldt Berlingskes nyhedsbrev.	
-					</div>
-					
-				</div>
-
-			</aside>
-
-
-
-			<? while (dumb_luck("14")): ?><p><? dummy("text@paragraph") ?></p><? endwhile ?>      
+			
+			
 		</div>
 	</div>
 </div>
