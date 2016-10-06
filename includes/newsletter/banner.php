50a9ae67ecbec752bd9e8e9352325b3cf2f5853c
Bo Skakke <bo@skakke.dk>
2016-10-06 15:53:08 +0200

    newsletter

---
+++ includes/newsletter/banner.php
@@ -0,0 +1,16 @@
+<tr>
+	<td>
+		<table border="0" callpadding="0" cellspacing="0" width="100%" class="tableBanner">
+			<tr>
+				<td class="bannerHeader">Annonce</td>
+			</tr>
+			<tr>
+				<td class="bannerContent">
+					<a href="[BANNER-LINK]"><img src="https://ci3.googleusercontent.com/proxy/f2W_Rc56m9XGGF9ZvI9Ot8moQWEBKG4CvSM_1_ZcVVsMoeOPEsIUrgheE4oinHowiPUFVanZxUaJ_lvFmvU8rQQ60VA9MPh1VJYM4v2IT8NZIflXE078i7rk0sHGW8msuwBq0t1AfCFOgJKpSffEwgYBRV2F5A_hh_X7vfnl3HOav47ngaMPaTR1zOQGAc4S_0hv7x4kS6O_Tprs_qUQoUkBgnUd=s0-d-e1-ft#http://ab168007.adbutler-meson.com/adserve/;ID=168007;size=614x119;setID=205982;type=img;uid=e3893a9e7aba93c35de14067ae8adea3;cache=1475733600" style="max-width:614px;"></a>
+				</td>
+				
+			</tr>
+		</table>
+		
+	</td>
+</tr>
---
+++ includes/newsletter/teasers-2-col.php
@@ -0,0 +1,36 @@
+<table border="0" cellpadding="20" cellspacing="0" width="100%" id="templateColumns">
+	<tr mc:repeatable>
+    	<td align="left" valign="top" style="padding-bottom:0;">
+        	<table align="left" border="0" cellpadding="0" cellspacing="0" class="templateColumnContainer">
+            	<tr>
+                	<td class="leftColumnContent">
+                		<a href="[ARTICLE LINK]"><img src="<? dummy("image@534x,16:9") ?>" class="columnImage"/></a>
+                    </td>
+                </tr>
+                <tr>
+                	<td valign="top" class="leftColumnContent" mc:edit="left_column_content">
+                        <h3>
+                        	Politiko
+                        </h3>
+                        <h2><a href="[ARTICLE LINK]"><? dummy("text@headline") ?></a></h2>
+                    </td>
+                </tr>
+            </table>
+        	<table align="right" border="0" cellpadding="0" cellspacing="0" class="templateColumnContainer">
+            	<tr>
+                	<td class="rightColumnContent">
+                    	<img src="<? dummy("image@534x,16:9") ?>" class="columnImage"/>
+                    </td>
+                </tr>
+                <tr>
+                	<td valign="top" class="rightColumnContent" mc:edit="right_column_content">
+                		<h3>
+                			Politiko
+                		</h3>
+                        <h2><a href="[ARTICLE LINK]"><? dummy("text@headline") ?></a></h2>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
