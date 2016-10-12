69fe4cd0775d5f973ba6ef9e53d1ebf25eb40bd3
Bo Skakke <bo@skakke.dk>
2016-10-12 09:01:47 +0200

    white background

--- includes/cookie-warning.php
+++ includes/cookie-warning.php
@@ -3,9 +3,8 @@
 <div class="cookie-warning">
 	<div class="container">
 		
-				<p>Websitet anvender cookies til at huske dine indstillinger, statistik og at målrette annoncer. Denne information deles med tredjepart. <a href="http://www.berlingskemedia.dk/cookie-og-privatlivspolitik" target="_blank">Læs mere </a>
-				<button class="btn btn-sm btn-blue">OK</button></p>
-		
+				<p><span></span> Websitet anvender cookies til at huske dine indstillinger, statistik og at målrette annoncer. Denne information deles med tredjepart. <a href="http://www.berlingskemedia.dk/cookie-og-privatlivspolitik" target="_blank">Læs mere </a></p>
+				<button class="btn btn-sm btn-blue">Jeg accepterer</button>
 	</div>
 	
 </div>
