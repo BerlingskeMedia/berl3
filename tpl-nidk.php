69f45c71c6ced6858de1e62de679c251eae3f8b5
Bo Skakke <bo@skakke.dk>
2016-11-15 13:12:52 +0100

    title

--- tpl-nidk.php
+++ tpl-nidk.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>BT - ni.dk</title>
+	<title>Berlingske - ni.dk</title>
 	<link rel="stylesheet" type="text/css" href="http://www.ni.dk/partner/partner-css/partner.css" />
 	<link rel="stylesheet" type="text/css" href="/dist/styles/nidk_iframe.css"/>
 </head>
